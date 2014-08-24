45bc95405bdc12a0d240ec9d33e102e4f95cce19
bcambl <blayne@blaynecampbell.com>
2014-08-24 14:40:27 -0600

    update example settings file with ntp information

--- example-settings.py
+++ example-settings.py
@@ -5,6 +5,7 @@ network = 'test_files/network-sample'  # network
 p_ifcfg = 'test_files/p_ifcfg-sample'  # ifcfg-eth0
 b_ifcfg = 'test_files/b_ifcfg-sample'  # ifcfg-eth1
 persistent = 'test_files/persistent-sample'  # 70-persistent-net.rules
+ntpconf = 'test_files/ntp-sample'  # ntp.conf
 resolvconf = 'test_files/resolv-sample'  # resolve.conf
 #### Development End #
 
@@ -15,6 +16,7 @@ resolvconf = 'test_files/resolv-sample'  # resolve.conf
 # p_ifcfg = '/etc/sysconfig/network-scripts/ifcfg-eth0'
 # b_ifcfg = '/etc/sysconfig/network-scripts/ifcfg-eth1'
 # persistent = '/etc/udev/rules.d/70-persistent-net.rules'
+# ntpconf = '/etc/ntp.conf'
 # resolvconf = '/etc/resolv.conf'  # resolve.conf
 
 #### Production End #
@@ -25,3 +27,10 @@ nameservers = ['4.4.4.4',  # google
                '208.67.222.222',  # opendns
                '208.67.220.220']  # opendns
 #### Nameservers End #
+
+#### Network Time Servers (NTP) #
+ntpservers = ['0.pool.ntp.org',
+              '1.pool.ntp.org',
+              '2.pool.ntp.org',
+              '3.pool.ntp.org']
+#### NTP End #
